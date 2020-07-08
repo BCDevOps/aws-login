de46456c1eca36f07cb6d22a3f1c5e851650a6ab
Ryan Jaeger <rjjaeger@amazon.com>
2020-07-08 08:57:28 -0700

    add tagging enhancements

---
+++ terraform/aws/variables.tf
@@ -0,0 +1,8 @@
+variable keycloak_saml_name{
+    description = "Keycloak IDP Name"
+    default     = "BCGovKeyCloak"
+}
+
+variable master_account_id {
+    description = "Master AWS Account Id"
+}
